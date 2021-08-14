f511614651b4ceed371de4fd410c9c3f8c8cff74
Theodor Wulff <theowulff@gmx.de>
2021-08-14 21:05:17 +0200

    almost runs

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import numpy as np
 import retro
 import os
 import torch.cuda
+from torch.nn import MSELoss
 from torch.utils.tensorboard import SummaryWriter
 
 from model import FeatureExtractor, Actor, Critic
+from ppo import PPO, GAE
 from utils import load_hparams
 from discretizer import SFDiscretizer
 from PIL import Image
+from torch.optim import Adam
+
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
+torch.autograd.set_detect_anomaly(True)
 
 def main():
     """This will be the main loop, creating the environment and starting the PPO algorithm"""
@@ -31,7 +35,12 @@ def main():
     env = SFDiscretizer(env)
 
     observation_length = params['observation_length']
-    explore_steps = params['explore_steps']
+    horizon = params['horizon']
+    num_epochs = params['epochs']
+
+    alpha = 1.0 # will be reduced during training, linearly annealed
+    ppo = PPO(params['ppo']['epsilon'], alpha, device)
+    gae = GAE(horizon, params['gae']['gamma'], params['gae']['gae_lambda'], device)
 
     print("Loading feature extractor...")
     feature_extractor = FeatureExtractor(device)
@@ -40,66 +49,103 @@ def main():
     actor = Actor(env.action_space.n)
     actor.train()
     actor.to(device)
+    actor_optim = Adam(actor.parameters(), lr=params['learning_rate'])
 
     print("Loading critic...")
     critic = Critic()
     critic.train()
     critic.to(device)
+    critic_optim = Adam(critic.parameters(), lr=params['learning_rate'])
 
     print("Starting main loop")
-    done = False
     global_step = 0
     obs = env.reset()
     obs = Image.fromarray(obs, 'RGB')
 
-    actions = torch.zeros(explore_steps).to(device)
-    action_dists = torch.zeros(explore_steps, env.action_space.n).to(device)
-    actions_one_hot = torch.zeros(explore_steps, env.action_space.n).to(device)
-    q_values = torch.zeros(explore_steps).to(device)
-    masks = torch.tensor([False] * explore_steps, dtype=torch.bool).to(device)
-    states = torch.zeros(explore_steps, 512).to(device)
-    rewards = torch.tensor(explore_steps).to(device)
+    # init policy values for current observations
+    actions = torch.zeros(horizon, device=device)
+    action_dists = torch.zeros(horizon, env.action_space.n, device=device)
+    log_probs = torch.zeros(horizon, device=device)
+    q_values = torch.zeros(horizon, device=device)
+    masks = torch.zeros(horizon, device=device)
+    states = torch.zeros(horizon, 512, device=device)
+    rewards = torch.zeros(horizon, device=device)
 
-    ### Gather experience
-    for step in range(explore_steps):
-        # choose best action according to policy
-        features = feature_extractor.extract_features(obs)
-        action_dist = actor(features)
-        action = torch.multinomial(action_dist, 1)  # sample action based on probabilities from softmax output of actor
+    # init ground truth policy values before update
+    batch_log_probs = torch.zeros(horizon, device=device)
+    batch_advantage_estimates = torch.zeros(horizon, device=device)
 
-        # collect a few frames and calculate mean based on action
-        obs, rew, done, _info = env.step(action)
-        obs = obs.astype('float')
-        num_obs = 0
-        for i in range(observation_length - 1):
-            nobs, nrew, ndone, _info = env.step(action)
-            nobs = nobs.astype('float')
-            obs += nobs
-            rew += nrew
-            num_obs += 1
-            if ndone:
-                done = True
-                break
-        obs /= num_obs
-        obs = Image.fromarray(obs.astype('uint8'), 'RGB')
+    for epoch in range(num_epochs):
+        print(f"-------- Epoch {epoch} -------------")
+        for i_update in range(params['updates_per_epoch']+1): # adding one since first run collects samples
+            if i_update > 0:
+                print(f"Update {i_update}")
+            else:
+                print("Gathering comparison batch")
 
-        # store information gained from current step
-        q_values[step] = critic(features)
-        states[step] = features
-        actions[step] = action
-        action_dists[step] = action_dist
-        actions_one_hot[step, action] = 1
-        rewards[step] = rew
-        masks[step] = not done
+            ### Gather experience
+            for step in range(horizon):
+                # choose best action according to policy
+                features = feature_extractor.extract_features(obs)
+                action_dist = actor(features)
+                action = torch.multinomial(action_dist, 1)  # sample action based on probabilities from softmax output of actor
 
-        if params['log_results']:
-            log.add_scalar("Reward", rew, global_step)
+                # collect a few frames and calculate mean based on action
+                obs, rew, done, _info = env.step(action)
+                rew /= 176.0
+                obs = obs.astype('float')
+                num_obs = 0
+                for i in range(observation_length - 1):
+                    nobs, nrew, ndone, _info = env.step(action)
+                    nrew /= 176.
+                    nobs = nobs.astype('float')
+                    obs += nobs
+                    rew += nrew
+                    num_obs += 1
+                    if ndone:
+                        done = True
+                        break
+                obs /= num_obs
+                obs = Image.fromarray(obs.astype('uint8'), 'RGB')
 
-        env.render()
-        if done:
-            env.reset()
+                # store information gained from current step
+                q_values[step] = critic(features)
+                states[step] = features
+                actions[step] = action
+                action_dists[step] = action_dist
+                rewards[step] = rew
+                masks[step] = 0 if done else 1
+                log_probs[step] = torch.log(action_dist[action])
 
-        global_step += 1
+                if params['log_results']:
+                    log.add_scalar("Reward", rew, global_step)
+
+                if done:
+                    env.reset()
+
+                global_step += 1
+
+            if i_update == 0:
+                # collected first batch -> reference policy
+                batch_log_probs = log_probs.clone()
+                batch_advantage_estimates, _ = gae.generalized_advantage_estimation(q_values, rewards, masks)
+            else:
+                # update step
+                _, advantage_estimates = gae.generalized_advantage_estimation(q_values, rewards, masks)
+
+                print("Updating actor")
+                actor_loss = ppo.clipped_surrogate_loss(log_probs, batch_log_probs, advantage_estimates)
+                actor_optim.zero_grad()
+                actor_loss.backward(retain_graph=True)
+                actor_optim.step()
+
+                print("Updating critic")
+                critic_loss = MSELoss()(q_values, batch_advantage_estimates)
+                critic_optim.zero_grad()
+                critic_loss.backward()
+                critic_optim.step()
+
+    ppo.alpha -= 1.0/num_epochs
 
     # obs.show()
     env.close()
--- ppo.py
+++ ppo.py
@@ -1,3 +1,35 @@
+import torch
+
+class GAE:
+    def __init__(self, horizon, gamma, gae_lambda, device):
+        self.horizon = horizon
+        self.gamma = gamma
+        self.gae_lambda = gae_lambda
+        self.device = device
+
+    def generalized_advantage_estimation(self, q_values, rewards, masks):
+        advantage_estimates = torch.zeros(self.horizon, device=self.device)
+        advantage_estimates[self.horizon-1] = rewards[self.horizon-1] - q_values[self.horizon-1]
+        for i in range(self.horizon-2, -1, -1):
+            # masks excludes q_values after done condition has reached because next state would be after reset
+            delta = rewards[i] + self.gamma * q_values[i + 1] * masks[i] - q_values[i]
+            advantage_estimates[i] = delta + self.gamma * self.gae_lambda * advantage_estimates[i+1] * masks[i]
+
+        normalized_estimates = (advantage_estimates - torch.mean(advantage_estimates)) / (torch.std(advantage_estimates) + 1e-10)
+        return advantage_estimates, normalized_estimates
+
 class PPO:
-    def __init__(self):
-        pass
+    def __init__(self, epsilon, alpha, device):
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.device = device
+
+    def clipped_surrogate_loss(self, probs, old_probs, advantage_estimates):
+        # expecting log probabilities for the next line to hold
+        probability_ratio = torch.exp(probs - old_probs)
+        clipped = torch.clamp(probability_ratio, 1-self.epsilon*self.alpha, 1+self.epsilon*self.alpha) * advantage_estimates
+        unclipped = probability_ratio * advantage_estimates
+        return torch.mean(-torch.min(unclipped, clipped))
+
+
+
